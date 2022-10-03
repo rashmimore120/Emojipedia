557283fb6eaa0965ffac2a8c10dbb14ab4413849
Rashmimore <rashmimore120@gmail.com>
2022-10-03 14:17:17 +0530

    Updates

---
+++ precache-manifest.a7d9d19815e8c1089554984354951999.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c5dbd6b9bfb112e8dd3b06cf05dd98",
+    "url": "/rashmimore120/Emojipedia/index.html"
+  },
+  {
+    "revision": "827efb21112e19d640a8",
+    "url": "/rashmimore120/Emojipedia/static/js/2.056525c0.chunk.js"
+  },
+  {
+    "revision": "ad61273ec793ac977f1a",
+    "url": "/rashmimore120/Emojipedia/static/js/main.720a5b70.chunk.js"
+  },
+  {
+    "revision": "a05651162d11afac54a0",
+    "url": "/rashmimore120/Emojipedia/static/js/runtime-main.03f1775b.js"
+  }
+]);
